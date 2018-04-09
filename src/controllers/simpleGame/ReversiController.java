d78a48f55e1fb4f6a12a6132195c4e15a378dbd5
Robbert Batterink <r.t.batterink@st.hanze.nl>
2018-04-09 14:03:49 +0200

    laaste regel reversi

--- src/controllers/simpleGame/ReversiController.java
+++ src/controllers/simpleGame/ReversiController.java
@@ -1,6 +1,6 @@
 package controllers.simpleGame;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashMap;
 import java.util.List;
 import javafx.stage.Stage;
@@ -10,6 +10,7 @@ import views.GameView;
 public class ReversiController extends SimpleGameController{
     //private Game gameModel;
     List<String> directions = new ArrayList<String>();
+    List<Integer> changeindex = new ArrayList<Integer>();
 
 public ReversiController(Game model, Stage primaryStage, GameView gameView, HashMap info) {
         super(model, primaryStage, gameView, info);
@@ -23,7 +24,7 @@ public ReversiController(Game model, Stage primaryStage, GameView gameView, Hash
     }
 
    public boolean legalMove(int index) {
-        if (super.legalMove(index) && nextToEnemy(index))
+        if (super.legalMove(index) && nextToEnemy(index) && friendlyInLine(index))
             return true;
         return false;
     }
@@ -57,10 +58,10 @@ public ReversiController(Game model, Stage primaryStage, GameView gameView, Hash
 	directions.add("NORTH-EAST");
     }
     if(gameModel.getPlayFieldAtIndex(index + 9) == 2){
-	directions.add("WEST-EAST");
+	directions.add("SOUTH-EAST");
     }
     if(gameModel.getPlayFieldAtIndex(index - 9) == 2){
-	directions.add("NORTH-SOUTH");
+	directions.add("NORTH-WEST");
     }
 	return !(directions.isEmpty());
     }
@@ -81,17 +82,160 @@ public ReversiController(Game model, Stage primaryStage, GameView gameView, Hash
 	}   
     }
     
-    public boolean checkDiagonal(index){
-        
+    public boolean checkDiagonal(int index){
+        int current = index;
+	if(directions.contains("SOUTH-WEST")){
+	    try{
+		while(!(current % 8 == 0)){
+		    if(gameModel.getPlayFieldAtIndex(current) == 2){
+			changeindex.add(current);
+		    } else if(gameModel.getPlayFieldAtIndex(current) == 1){
+			current = index;
+			break;
+		    } else {
+			changeindex.clear();
+			current = index;
+		    }
+		    current -= 7;
+		}
+	    }catch(ArrayIndexOutOfBoundsException e){
+	    changeindex.clear();
+	    }
+	}
+	
+	if(directions.contains("NORTH-WEST")){
+	    try{
+		while(!(current % 8 == 0)){
+		    if(gameModel.getPlayFieldAtIndex(current) == 2){
+			changeindex.add(current);
+		    } else if(gameModel.getPlayFieldAtIndex(current) == 1){
+			current = index;
+			break;
+		    } else {
+			changeindex.clear();
+			current = index;
+		    }
+		    current -= 9;
+		}
+	    }catch(ArrayIndexOutOfBoundsException e){
+	    changeindex.clear();
+	    }
+	}
+		
+	if(directions.contains("SOUTH-EAST")){
+	    try{
+		while(!(current % 8 == 0)){
+		    if(gameModel.getPlayFieldAtIndex(current) == 2){
+			changeindex.add(current);
+		    } else if(gameModel.getPlayFieldAtIndex(current) == 1){
+			current = index;
+			break;
+		    } else {
+			changeindex.clear();
+			current = index;
+		    }
+		    current += 9;
+		}
+	    }catch(ArrayIndexOutOfBoundsException e){
+	    changeindex.clear();
+	    }
+	}
+			
+	if(directions.contains("NORTH-EAST")){
+	    try{
+		while(!(current % 8 == 0)){
+		    if(gameModel.getPlayFieldAtIndex(current) == 2){
+			changeindex.add(current);
+		    } else if(gameModel.getPlayFieldAtIndex(current) == 1){
+			current = index;
+			break;
+		    } else {
+			changeindex.clear();
+			current = index;
+		    }
+		    current -= 9;
+		}
+	    }catch(ArrayIndexOutOfBoundsException e){
+	    changeindex.clear();
+	    }
+	}
+	return !(changeindex.isEmpty());
     }
     
-    public boolean checkVertical(index) {
-    
-    }
+    public boolean checkVertical(int index) {
+        int current = index;
+        if (directions.contains("NORTH")) {
+            try {
+                while(true) {
+                    if (gameModel.getPlayFieldAtIndex(current) == 1) {
+                        break;
+                    } else if (gameModel.getPlayFieldAtIndex(current) == 0) {
+                        changeindex.clear();
+                        break;
+                    } else {
+                        changeindex.add(current);
+                        current -= 8;
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                changeindex.clear();
+            }
+        }
+        current = index;
+        if (directions.contains("SOUTH")) {
+            try {
+                while(true) {
+                    if (gameModel.getPlayFieldAtIndex(current) == 1) {
+                        break;
+                    } else if (gameModel.getPlayFieldAtIndex(current) == 0) {
+                        changeindex.clear();
+                        break;
+                    } else {
+                        changeindex.add(current);
+                        current += 8;
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                changeindex.clear();
+            }
+        }
+
+        return !(changeindex.isEmpty());
+    } 
+
     
     // 8, 9, 10, 11, 12, 13, 14, 15
     public boolean checkHorizontal(int index) {
 	int current = index;
+	if(directions.contains("WEST")){
+	    while(!(current % 8 == 0)){
+		if(gameModel.getPlayFieldAtIndex(current) == 2){
+		    changeindex.add(current);
+		} else if(gameModel.getPlayFieldAtIndex(current) == 1){
+		    current = index;
+		    break;
+		} else {
+		    changeindex.clear();
+		    current = index;
+		}
+		current--;
+	    }
+	}
 	
+	if(directions.contains("EAST")) {
+	    while(!(current + 1 % 8 == 0)){
+		if(gameModel.getPlayFieldAtIndex(current) == 2){
+		    changeindex.add(current);
+		} else if(gameModel.getPlayFieldAtIndex(current) == 1){
+		    current = index;
+		    break;
+		} else {
+		    changeindex.clear();
+		    current = index;
+		}
+		current++;
+	    }
+	} 
+	return !(changeindex.isEmpty());
     }
 }
