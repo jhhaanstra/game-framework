89da68a58885dfff1b5888cc36ba97aaffb116b1
Pascal Vellema <pascalvellema@gmail.com>
2018-04-18 20:43:14 +0200

    JavaDoc + Refactor (Client, Server & ClientCommands)

--- src/models/Client.java
+++ src/models/Client.java
@@ -8,8 +8,6 @@ import javafx.application.Platform;
 
 public class Client extends Server {
     private static Client client;
-    private String ip;
-    private int port;
     private static LinkedList<String> errors = new LinkedList<>();
     private static LinkedList<String> info = new LinkedList<>();
     private static LinkedList<String> match = new LinkedList<>();
@@ -19,10 +17,13 @@ public class Client extends Server {
     private static LinkedList<String> turn = new LinkedList<>();
     private static LinkedList<String> playerlist = new LinkedList<>();
 
-
+    /**
+     * Hier wordt een nieuwe instantie van de Singleton Client aangemaakt wanneer deze er nog niet is.
+     * Is er al een instantie van Client dan wordt deze meegegeven in de return.
+     */
     public static Client getInstance() {
         if (client == null) {
-            client = new Client("127.0.0.1", 7789, (data -> {
+            client = new Client((data -> {
                 if (data.contains("ERR")) errors.add(data);
                 if (data.contains("SVR")) info.add(data);
                 if (data.contains("SVR PLAYERLIST")) playerlist.add(data);
@@ -36,10 +37,14 @@ public class Client extends Server {
         return client;
     }
 
-    private Client(String ip, int port, Consumer<String> onReceiveCallback) {
+
+    /**
+     * De constructor van de Client
+     * @param onReceiveCallback
+     * Deze wordt meegegeven aan de constructor van de super klasse (Server)
+     */
+    private Client(Consumer<String> onReceiveCallback) {
         super(onReceiveCallback);
-        this.ip = ip;
-        this.port = port;
     }
 
     public LinkedList<String> getErorrs() {
--- src/models/Server.java
+++ src/models/Server.java
@@ -11,33 +11,56 @@ public abstract class Server {
     private Server.ConnectionThread connThread = new Server.ConnectionThread();
     private Consumer<String> onReceiveCallback;
 
+    /**
+     * De constructor van de Server
+     * @param onReceiveCallback
+     * Deze is afkomstig van de subklasse Client.
+     * Maak een Daemon thread
+     */
     public Server(Consumer<String> onReceiveCallback) {
         this.onReceiveCallback = onReceiveCallback;
         connThread.setDaemon(true);
     }
 
+    /**
+     * Start de connection Thread.
+     */
     public void startConnection() throws Exception {
         connThread.start();
     }
 
+    /**
+     * Deze functie stuurt een bericht naar de server.
+     * @param data
+     * Data is de String die je naar de server wil versturen.
+     */
     public void send(String data) {
         try {
-            System.out.println("C: " + data);
             connThread.out.println(data);
         } catch (Exception e) {
-            System.out.println("Zenden wil niet..");
             e.printStackTrace();
         }
     }
 
+    /**
+     * Deze functie stopt de connectie.
+     */
     public void closeConnection() throws Exception {
         connThread.socket.close();
     }
 
+    /**
+     * Dit is een inner class ConnectionThread die Thread extends.
+     */
     private class ConnectionThread extends Thread {
         private Socket socket;
         private PrintWriter out;
 
+        /**
+         * De run methode van deze inner class.
+         * In de try wordt de connnectie met de server opgezet, een PrintWriter en InputStreamReader.
+         * Er wordt constant geluisterd of er berichten worden ontvangen.
+         */
         @Override
         public void run() {
             try(Socket socket = new Socket("localhost", 7789);
@@ -59,7 +82,6 @@ public abstract class Server {
             }
 
             catch (IOException e) {
-                System.out.println("Server start niet");
                 e.printStackTrace();
             }
         }
